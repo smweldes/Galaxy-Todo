280a9158df38cb33dfacc1c7c755487fca420724
smweldes <weldesiem@gmail.com>
2021-11-02 20:43:28 -0700

    add todos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import { Route, Switch } from 'react-router';
 import Navbar from './components/Navbar';
+import AddTodo from './components/AddTodo';
 
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
       <Navbar/>
      <Switch>
       <Route exact path="/" component={()=><Home/>}/>
+
+      <Route exact path="/add" >
+      <AddTodo/>
+        </Route>
       
     </Switch>
     </div>
